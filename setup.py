8f5084b65cfc355598319d0554e38278f7ef3d98
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2022-10-27 15:00:30 +0200

    Add basic setup.py and tests entrypoint

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+base_requires = [
+    'jax>=0.3.23',
+]
+test_requires = [
+    'pytest',
+]
+
+setuptools.setup(
+    name="macx",
+    version="0.1.0",
+    author="The macx gang",
+    author_email="",
+    license="ASL",
+    install_requires=base_requires,
+    extras_require={'test': test_requires},
+    packages=setuptools.find_packages(),
+    url="https://github.com/sirmarcel/macx",
+    python_requires='>=3.8',
+)
