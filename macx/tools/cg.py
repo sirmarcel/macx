de054f449b5bf0a95d2c97ed395fc2b0b86313ae
Mihail Bogojeski <mihailbogojeski@gmail.com>
2022-10-27 16:59:48 +0200

    fixed some bugs with the wrapper and test functions

--- macx/tools/cg.py
+++ macx/tools/cg.py
@@ -1,23 +1,24 @@
 import mace
-from e3nn_jax import Irreps
+from e3nn_jax import Irreps, Irrep
 from macx.tools import cg
 from mace.tools import cg as mace_cg
 from e3nn import o3
 import jax.numpy as jnp
 
-def U_matrix_real_easymode(
+def U_matrix_real( #easymode
     irreps_in, #: Union[str, o3.Irreps],
     irreps_out, #: Union[str, o3.Irreps],
-    correlation, #: int,
-    normalization, #: str = "component",
+    correlation: int,
+    normalization: str = "component",
     filter_ir_mid=None,
     dtype=None,
 	):
 	irreps_in_o3 = o3.Irreps(str(irreps_in))
 	irreps_out_o3 = o3.Irreps(str(irreps_out))
-	U_matrix_torch = mace_cd.U_matrix_real(irreps_in=irreps_in_o3, irreps_out=irreps_out_o3,
+	U_matrix_torch = mace_cg.U_matrix_real(irreps_in=irreps_in_o3, irreps_out=irreps_out_o3,
 						  correlation=correlation, normalization=normalization,
 						  filter_ir_mid=filter_ir_mid)
-	U_matrix_jax = jnp.array(U_matrix_torch.cpu().detach().numpy(), dype=dtype) 
+	U_matrix_jax = [Irrep(str(U_mat)) if isinstance(U_mat, o3.Irrep) else jnp.array(U_mat.cpu().detach().numpy(), dtype=dtype) for U_mat in U_matrix_torch]
+	return U_matrix_jax
 	
 	
--- tests/test_cg.py
+++ tests/test_cg.py
@@ -22,12 +22,16 @@ def test_U_matrix_shape():
 def test_U_matrix_mace_compare():
     irreps_in = Irreps("1x0e + 1x1o + 1x2e")
     irreps_out = Irreps("1x0e + 1x1o")
+    print('initialized irreps jax')
     irreps_in_mace = o3.Irreps("1x0e + 1x1o + 1x2e")
     irreps_out_mace = o3.Irreps("1x0e + 1x1o")
+    print('initialized irreps e3nn')
     u_matrix = cg.U_matrix_real(
         irreps_in=irreps_in, irreps_out=irreps_out, correlation=3
     )[-1]
+    print('initialized matrix jax')
     u_matrix_mace = mace_cg.U_matrix_real(
         irreps_in=irreps_in_mace, irreps_out=irreps_out_mace, correlation=3
     )[-1]
+    print('initialized matrix e3nn')
     assert jnp.allclose(u_matrix, u_matrix_mace.cpu().detach().numpy())
