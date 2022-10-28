7f4aaf94b63c59af1efe710e3d7146adc40c50ee
Szabo Bernat <peter.bernat.szabo@fu-berlin.de>
2022-10-28 15:08:48 +0200

    fix black formatting

--- tests/test_symmetric_contraction.py
+++ tests/test_symmetric_contraction.py
@@ -10,7 +10,7 @@ import jax
 
 def test_symmetric_contraction_1():
     rng = jax.random.PRNGKey(5243)
-    
+
     irreps_in = o3.Irreps("7x0y+7x1y+7x2y")
     irreps_out = o3.Irreps("0y")
     correlation = 2
@@ -24,18 +24,16 @@ def test_symmetric_contraction_1():
         shared_weights=True,
     )
     torch_B = sym_con(A_torch, None)
-    
+
     irreps_in_jax = e3nn_jax.Irreps(str(irreps_in))
     irreps_out_jax = e3nn_jax.Irreps(str(irreps_out))
     A_jax = jnp.array(A_torch)
-    
-    
+
     @hk.without_apply_rng
     @hk.transform
     def sym_con_jax(A):
         return SymContr_jax(irreps_in_jax, irreps_out_jax, correlation)(A)
-    
-    
+
     params = sym_con_jax.init(rng, A_jax)
     params = {
         "symmetric_contraction/~/Contraction_1x0e": {
@@ -45,5 +43,7 @@ def test_symmetric_contraction_1():
     }
     params = jax.tree_util.tree_map(lambda x: jnp.array(x), params)
     jax_B = sym_con_jax.apply(params, A_jax)
-    
-    assert jnp.isclose(jnp.abs(jnp.array(torch_B.detach()) - jax_B).sum(), jnp.array(0.), atol=1.e-5)
+
+    assert jnp.isclose(
+        jnp.abs(jnp.array(torch_B.detach()) - jax_B).sum(), jnp.array(0.0), atol=1.0e-5
+    )
