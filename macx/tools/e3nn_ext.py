29ba428864aeaa24c0a0fe7545c2767ba40c80fa
Zeno Schätzle <zenoone@physik.fu-berlin.de>
2022-11-01 16:05:09 +0100

    add wrapped linear e3nn layer for arrays

---
+++ macx/tools/e3nn_ext.py
@@ -0,0 +1,20 @@
+from e3nn_jax import IrrepsArray, Linear
+
+
+def get_irreps_str(irreps, embedding_dim):
+    return "+".join(map(lambda x: f"{embedding_dim}x{x}", irreps))
+
+
+class ArrayLinear(Linear):
+    def __init__(self, irreps_out, irreps_in, embedding_dim, channel_out=1):
+        mult_irreps_out = get_irreps_str(irreps_out, embedding_dim)
+        mult_irreps_in = get_irreps_str(irreps_in, embedding_dim)
+        super().__init__(mult_irreps_out, channel_out, irreps_in=mult_irreps_in)
+        self.mult_irreps_in = mult_irreps_in
+        self.channel_out = channel_out
+
+    def __call__(self, x):
+        *leading_dims, embedding_dim, irreps_dim = x.shape
+        x = IrrepsArray(self.mult_irreps_in, x.reshape(*leading_dims, 1, -1))
+        out = super().__call__(x)
+        return out.array.reshape(*leading_dims, self.channel_out, embedding_dim, -1)
